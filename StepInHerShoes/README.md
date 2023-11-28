8458546f9a2cac9706b04e62d1bb384f7a8d2b34
Pranjali Jadhav <53855017+pranjalijadhav13@users.noreply.github.com>
2023-11-27 21:14:00 -0500

    Delete StepInHerShoes directory
    Committed at wrong path

--- StepInHerShoes/README.md
+++
@@ -1,2 +0,0 @@
-# StepInHerShoes
-A website which empowers and encourages women with inspiring and life saving incidents. Also it emphasis on various campaigns and event concerned with the women issues. Also it brings about the awareness of the laws and rights related to women.
--- StepInHerShoes/laws.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$file = 'lawnright.pdf';
-	$filename = 'lawnright.pdf';
-	header('Content-type: application/pdf');
-	header('Content-Deposition: inline; filename="' . $filename . '"');
-	header('Content-Transfer-Encoding: binary');
-	header('Accept-Ranges: bytes');
-	@readfile($file);
-?>
